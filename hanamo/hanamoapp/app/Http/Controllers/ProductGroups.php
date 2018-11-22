6c20e6a7b4fc2f5fcac5ba3ed829c5587a92129f
jaremsalonga <fielsalonga@gmail.com>
2018-11-22 21:35:15 +0800

    updates

---
+++ hanamo/hanamoapp/app/Http/Controllers/ProductGroups.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\MProductGroups;
+
+class ProductGroups extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function viewProductGroups()
+    {
+        return view('product_group');
+    }
+
+    public function index()
+    {
+        //
+        $posts = MProductGroups::latest()->paginate(5);
+        return response()->json($posts);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $post = MProductGroups::create($request->all());
+        return response()->json($post);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $post = MProductGroups::find($id)->update($request->all());
+        return response()->json($post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        MProductGroups::find($id)->delete();
+        return response()->json(['done']);
+    }
+}
--- hanamo/hanamoapp/routes/web.php
+++ hanamo/hanamoapp/routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('product_group', 'ProductGroups@viewProductGroups');
+Route::resource('product_groups','ProductGroups');
+// Route::get('/product_group', 'ProductGroups@viewProductGroups');
+// Route::resource('product_groups','ProductGroups');
