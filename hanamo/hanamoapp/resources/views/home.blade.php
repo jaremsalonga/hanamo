eb15480a6d781006ef19a75d824479cb7f8901b3
jaremsalonga <fielsalonga@gmail.com>
2018-11-23 21:46:41 +0800

    updatesss

--- hanamo/hanamoapp/resources/views/home.blade.php
+++ hanamo/hanamoapp/resources/views/home.blade.php
@@ -20,7 +20,7 @@
                         <a href="/product" class="list-group-item">Product</a>
                         <a href="/product_group" class="list-group-item">Product Group</a>
                         <a href="/users" class="list-group-item">User</a>
-                        <a href="/excel" class="list-group-item">Import/Export Excel</a>
+                        <a href="/importexcel" class="list-group-item">Import/Export Excel</a>
                     </div>
                     @else
                     <div class="list-group">
